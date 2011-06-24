6091568cc5b5f6f4ac3154a322fcb2e06e25004d
Slamet Nurhadi <slametnhd@gmail.com>
2011-06-24 03:07:58 -0700

    initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sldemo::Application.config.secret_token = 'cd4ce1d403fb31fdb575dab12fd941b3cb3c6885ff72470e079e28a9fabb15cc0120324d14990d74eb15714ec1df353f379d9a574209fa5aa5dbd0e781587203'
